9cd6d1bfa2cc6c8ace7ae8664b47a1ed92bfc86a
Adam Oliveras <theadamoliveras@gmail.com>
2019-07-22 14:46:47 -0700

    completed chapter 5 excercise 2.

--- chapter-5_HOF/5-2_Your-Own-Loop.js
+++ chapter-5_HOF/5-2_Your-Own-Loop.js
@@ -1,5 +1,11 @@
 // Your code here.
-const loop = () => {};
+const loop = (value, test, update, body) => {
+  for (let i = value; i > 0; i--) {
+    if (!test) return;
+    body(i);
+    update(i);
+  }
+};
 
 loop(3, n => n > 0, n => n - 1, console.log);
 // → 3
